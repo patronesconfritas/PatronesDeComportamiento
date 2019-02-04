72ede855ebb5b21ac74a5eb1f0d582fb0ab2e892
Daniel Agustín Espinola <despinola@PL-SGILBERTO.cx.ar>
2019-02-04 15:39:33 -0300

    initial commit

---
+++ State/src/com/comportamiento/ejemplo1/app/App.java
@@ -0,0 +1,10 @@
+package com.comportamiento.ejemplo1.app;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ State/src/com/comportamiento/ejemplo1/entity/Foo.java
@@ -0,0 +1,5 @@
+package com.comportamiento.ejemplo1.entity;
+
+public class Foo {
+
+}
